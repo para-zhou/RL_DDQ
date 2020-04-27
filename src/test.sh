1f3dd4f6409650191c2fa50106b3c04e23de33cd
jinchao.li <jinchao.li@horizon.ai>
2020-04-27 08:16:17 +0800

    new file:   src/test.sh 	new file:   src/train.sh

---
+++ src/test.sh
@@ -0,0 +1,3 @@
+path=$1
+pkl=$2
+python run.py --agt 6 --usr 1 --max_turn 40  --movie_kb_path ./deep_dialog/data/movie_kb.1k.p  --dqn_hidden_size 80  --experience_replay_pool_size 1000  --episodes 300  --simulation_epoch_size 100  --write_model_dir ./deep_dialog/checkpoints/${path}/  --slot_err_prob 0.00  --intent_err_prob 0.00  --batch_size 16  --epsilon 0 --goal_file_path ./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p  --trained_model_path ./deep_dialog/checkpoints/${path}/${pkl}  --run_mode 3
