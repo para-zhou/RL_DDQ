b70db6ec6f4dcaf6de7f773b7a02ef10f172b293
JinchaoLove <jinchao@bdda1.itsc.cuhk.edu.hk>
2020-05-23 23:14:00 +0800

    run several times

---
+++ src/fig4_10.sh
@@ -0,0 +1,11 @@
+for ((i=1; i<= 5; i++));do
+let "seed=$i*100"
+python run.py --agt 6 \
+--usr 1 --max_turn 40 --movie_kb_path ./deep_dialog/data/movie_kb.1k.p --dqn_hidden_size 80 \
+--experience_replay_pool_size 5000 --episodes 500 --simulation_epoch_size 100 \
+--run_mode 3 --act_level 0 --slot_err_prob 0.0 --intent_err_prob 0.00 --batch_size 16 \
+--goal_file_path ./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p \
+--warm_start 1 --warm_start_epochs 100 \
+--write_model_dir ./deep_dialog/checkpoints/DDQ_k10_run$i \
+--planning_steps 10 --torch_seed $seed --grounded 0 --boosted 1 --train_world_model 1
+done
